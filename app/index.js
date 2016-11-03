8eb34ea35593d4c6ceee90d2e7513d74dd2c9d05
masted <masted311@gmail.com>
2016-11-03 13:01:47 +0300

    first dev release

--- app/index.js
+++ app/index.js
@@ -1,3 +1,15 @@
+Array.prototype.clean = function(deleteValue) {
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] == deleteValue) {
+      this.splice(i, 1);
+      i--;
+    }
+  }
+  return this;
+};
+
+// ----------
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -13,6 +25,21 @@ var jwt = require('jsonwebtoken');
 var socketioJwt = require('socketio-jwt');
 var jwtSecret = 'asd';
 
+function dirname (path) {
+  //  discuss at: http://locutus.io/php/dirname/
+  // original by: Ozh
+  // improved by: XoraX (http://www.xorax.info)
+  //   example 1: dirname('/etc/passwd')
+  //   returns 1: '/etc'
+  //   example 2: dirname('c:/Temp/x')
+  //   returns 2: 'c:/Temp'
+  //   example 3: dirname('/dir/test/')
+  //   returns 3: '/dir'
+
+  return path.replace(/\\/g, '/')
+    .replace(/\/[^\/]*\/?$/, '')
+}
+
 mongoClient.connect(url, function(err, _db) {
   console.log('mongo connected');
   db = _db;
@@ -22,18 +49,25 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.use(express.static('/home/user/marsel/chat/doc'));
+app.use(express.static(dirname(__dirname) + '/doc'));
 app.get('/doc', function(req, res) {
-  res.sendFile('/home/user/marsel/chat/doc/index.html');
+  res.sendFile(dirname(__dirname) + '/doc/index.html');
 });
 
+var push = function(user, msg) {
+
+};
+
 /**
- * @api {get} /login/:login/:password Login
+ * @api {get} /api/v1/login/:login/:password Login
+ * @apiDescription Token expiration time: 1 week
  * @apiName Login
  * @apiGroup Auth
  *
  * @apiParam {String} login User login
  * @apiParam {String} password User password
+ * @apiParam {String} device ios/android
+ * @apiParam {String} deviceToken (optional) Device Token for APN
  *
  * @apiSuccess {String} token Token that you will use in Socket.IO connection
  *
@@ -45,19 +79,25 @@ app.get('/doc', function(req, res) {
  *   HTTP/1.1 404 Not Found
  *   {"error": "no user"}
  *
- * @apiSampleRequest http://chat.311.su:3000/login
+ * @apiSampleRequest http://chat.311.su:8080/api/v1/login
  */
-app.get('/login', function(req, res) {
+app.get('/api/v1/login', function(req, res) {
   db.collection('users').findOne({
     login: req.query.login,
     password: req.query.password
   }, function(err, profile) {
-    if (!profile) {
-      res.status(404).json({error: 'no user'});
-    } else {
-      var token = jwt.sign(profile, jwtSecret, {expiresIn: 60 * 5});
-      res.json({token: token});
+    if (req.query.device != 'ios' && req.query.device != 'android') {
+      res.status(404).json({error: 'wrong device'});
+      return;
     }
+    if (profile === null) {
+      res.status(404).json({error: 'no user'});
+      return;
+    }
+    profile.device = req.query.device;
+    profile.deviceToken = req.query.deviceToken;
+    var token = jwt.sign(profile, jwtSecret, {expiresIn: 60 * 60 * 24 * 7});
+    res.json({token: token});
   });
 });
 
@@ -81,7 +121,7 @@ app.get('/login', function(req, res) {
 //});
 
 /**
- * @api {get} /user/info/:phone Get user info
+ * @api {get} /api/v1/user/info/:phone Get user info
  * @apiName GetUserInfo
  * @apiGroup User
  *
@@ -93,9 +133,9 @@ app.get('/login', function(req, res) {
  *   HTTP/1.1 404 Not Found
  *   {"error": "error message"}
  *
- * @apiSampleRequest http://chat.311.su:3000/user/info
+ * @apiSampleRequest http://chat.311.su:8080/api/v1/user/info
  */
-app.get('/user/info', function(req, res) {
+app.get('/api/v1/user/info', function(req, res) {
   console.log('get info');
   console.log(req.query);
   db.collection('users').findOne({
@@ -113,7 +153,7 @@ app.get('/user/info', function(req, res) {
 });
 
 /**
- * @api {get} /user/create/:phone/:login/:password Create user
+ * @api {get} /api/v1/user/create/:phone/:login/:password Create user
  * @apiName Create
  * @apiGroup User
  *
@@ -127,9 +167,9 @@ app.get('/user/info', function(req, res) {
  *   HTTP/1.1 404 Not Found
  *   {"error": "error message"}
  *
- * @apiSampleRequest http://chat.311.su:3000/user/create
+ * @apiSampleRequest http://chat.311.su:8080/api/v1/user/create
  */
-app.get('/user/create', function(req, res) {
+app.get('/api/v1/user/create', function(req, res) {
   if (!req.query.phone) {
     res.status(404).json({error: 'phone is required'});
     return;
@@ -169,34 +209,145 @@ app.get('/user/create', function(req, res) {
   });
 });
 
+var clean = function(obj) {
+  for (var propName in obj) {
+    if (!obj[propName]) {
+      delete obj[propName];
+    }
+  }
+};
+
+var tokenReq = function(req, res, resCallback) {
+  jwt.verify(req.query.token, jwtSecret, function(err, decoded) {
+    if (!err) {
+      resCallback(res, decoded);
+    } else {
+      res.send(err);
+    }
+  });
+};
+
 /**
- * @api {get} /message/send/:fromUser/:toUser/:message Send a message
+ * @api {get} /api/v1/user/update/:name/:surname/:password Update user
+ * @apiName Update
+ * @apiGroup User
+ *
+ * @apiParam {String} token JWT token
+ * @apiParam {String} login Login
+ * @apiParam {String} phone Phone
+ * @apiParam {String} name Name
+ * @apiParam {String} surname Surname
+ *
+ * @apiSuccess {String} success The string "success" on success
+ *
+ * @apiErrorExample Error-Response:
+ *   HTTP/1.1 404 Not Found
+ *   {"error": "error message"}
+ *
+ * @apiSampleRequest http://chat.311.su:8080/api/v1/user/update
+ */
+app.get('/api/v1/user/update', function(req, res) {
+  tokenReq(req, res, function(res, user) {
+    var data = req.query;
+    delete data.token;
+    clean(data);
+    db.collection('users').update({_id: ObjectID(user._id)}, {$set: data}, function(err, count) {
+      if (count) res.send('success');
+      else res.json({error: 'use not found'});
+    });
+  });
+});
+
+// ------------- upload section ----------------
+
+var formidable = require('formidable');
+var fs = require('fs');
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * @api {post} /api/v1/user/upload Upload photo
+ * @apiName Upload photo
+ * @apiGroup User
+ *
+ * @apiParam {String} token JWT token
+ * @apiParam {File} image Image (File via multipart/form-data)
+ *
+ */
+app.post('/api/v1/user/upload', function(req, res) {
+  tokenReq(req, res, function(res, user) {
+    var form = new formidable.IncomingForm();
+    form.uploadDir = path.join(__dirname, '/uploads');
+    form.on('file', function(field, file) {
+      fs.rename(file.path, path.join(form.uploadDir, file.name));
+    });
+    form.on('error', function(err) {
+      console.log('An error has occured: \n' + err);
+    });
+    form.on('end', function() {
+      res.end('success');
+    });
+    form.parse(req);
+  });
+});
+
+// ----------------------------------------
+app.get('/api/v1/upload', function(req, res) {
+  res.sendFile(path.join(__dirname, 'upload.html'));
+});
+
+var apn = require('apn');
+
+/**
+ * @api {get} /api/v1/message/send/:fromUser/:toUser/:message Send a message
  * @apiName SendMessage
  * @apiGroup Message
  *
- * @apiParam {Number} fromUser Sender user ID
+ * @apiParam {String} token JWT token
  * @apiParam {Number} toUser Recipient user ID
  * @apiParam {String} message Message text
  *
  * @apiSuccess {String} success The string "success" on success
- * @apiSampleRequest http://chat.311.su:3000/message/send
+ * @apiSampleRequest http://chat.311.su:8080/api/v1/message/send
  */
-app.get('/message/send', function(req, res) {
-  db.collection('messages').insertOne({
-    fromUser: req.query.fromUser,
-    toUser: req.query.toUser,
-    message: req.query.message,
-    viewed: false
+app.get('/api/v1/message/send', function(req, res) {
+  tokenReq(req, res, function(res, user) {
+    db.collection('messages').insertOne({
+      fromUser: user._id,
+      toUser: req.query.toUser,
+      message: req.query.message,
+      viewed: false
+    });
+
+    io.in('events' + req.query.toUser).emit('newMessage', {
+      fromUser: req.query.fromUser
+    });
+
+    //var apnProvider = new apn.Provider({
+    //  token: {
+    //    key: "path/to/key.p8",
+    //    keyId: "T0K3NK3Y1D",
+    //    teamId: "T34M1D"
+    //  },
+    //  production: false
+    //});
+        //var note = new apn.Notification();
+    //note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+    //note.badge = 3;
+    //note.sound = "ping.aiff";
+    //note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+    //note.payload = {'messageFrom': 'Chat'};
+    //note.topic = "<your-app-bundle-id>";
+    //
+    //apnProvider.send(note, deviceToken).then(function(result) {});
+
+    res.send('success');
   });
-  console.log('sent');
-  io.in('events' + req.query.toUser).emit('newMessage', {
-    fromUser: req.query.fromUser
-  });
-  res.send('success');
 });
 
 /**
- * @api {get} /message/getNew/:fromUser/:toUser Getting new messages from chat (FOR DEBUG ONLY)
+ * @api {get} /api/v1/message/getNew/:fromUser/:toUser Getting new messages from chat (FOR DEBUG ONLY)
  * @apiName GetNew
  * @apiGroup Message
  *
@@ -204,9 +355,9 @@ app.get('/message/send', function(req, res) {
  * @apiParam {Number} toUser Recipient user ID
  *
  * @apiSuccess {json} New messages
- * @apiSampleRequest http://chat.311.su:3000/message/getNew
+ * @apiSampleRequest http://chat.311.su:8080/api/v1/message/getNew
  */
-app.get('/message/getNew', function(req, res) {
+app.get('/api/v1/message/getNew', function(req, res) {
   //
   var result = {};
   db.collection('messages').find({
@@ -240,18 +391,21 @@ app.get('/message/getNew', function(req, res) {
 });
 
 /**
- * @api {get} /message/markAsViewed/:_ids Mark as viewed
+ * @api {get} /api/v1/message/markAsViewed/:_ids Mark as viewed
  * @apiName MarkAsViewed
  * @apiGroup Message
  *
+ * @apiParam {String} token JWT token
  * @apiParam {String} _ids Object IDs separeted by quote ",". ID is the "_id" param of Mongo Document
- * @apiSampleRequest http://chat.311.su:3000/message/markAsViewed
+ * @apiSampleRequest http://chat.311.su:8080/api/v1/message/markAsViewed
  */
-app.get('/message/markAsViewed', function(req, res) {
-  var ids = req.query._ids.split(',');
-  db.collection('messages').updateMany({'_id': new ObjectID.createFromHexString('57ea54fd8a2fef5fea82041f')}, {$set: {viewed: true}}, function(err, r) {
-    console.log(r.result);
-    res.send(r.result.n ? 'success' : 'failed');
+app.get('/api/v1/message/markAsViewed', function(req, res) {
+  tokenReq(req, res, function(res, user) {
+    var ids = req.query._ids.split(',');
+    db.collection('messages').updateMany({'_id': new ObjectID.createFromHexString('57ea54fd8a2fef5fea82041f')}, {$set: {viewed: true}}, function(err, r) {
+      console.log(r.result);
+      res.send(r.result.n ? 'success' : 'failed');
+    });
   });
 });
 
@@ -285,6 +439,9 @@ io.sockets.on('connection', socketioJwt.authorize({
   timeout: 15000 // 15 seconds to send the authentication message
 })).on('authenticated', function(socket) {
   console.log('authenticated');
+
+  console.log(socket.decoded_token);
+
 
   socket.emit('event', {type: 'authenticated'});
 
@@ -341,6 +498,6 @@ io.sockets.on('connection', socketioJwt.authorize({
 
 });
 
-http.listen(3000, function() {
-  console.log('listening on *:3000');
+http.listen(8080, function() {
+  console.log('listening on *:8080');
 });
